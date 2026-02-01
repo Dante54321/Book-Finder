cd133ec91db7ee477243d8f30188129349936d46
David <davidalejmartinez@gmail.com>
2026-01-31 20:48:15 -0500

    Setting Up Spring Boot

---
+++ Backend/src/test/java/edu/fiu/book_finder/BookFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fiu.book_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
