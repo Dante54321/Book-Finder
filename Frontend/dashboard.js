3ac1b429fb8216c954ba3b71014609a420f2377a
Mia Mader <miamader2004@gmail.com>
2026-02-11 18:28:28 -0500

    additional modifications (login doesn't go through because of 'forbidden' error

--- Frontend/dashboard.js
+++ Frontend/dashboard.js
@@ -2,3 +2,15 @@ const token = localStorage.getItem('token');
 if (!token) {
     window.location.href = 'login.html'; // kick out user if not logged in
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const username = localStorage.getItem('username');
+    const welcomeHeader = document.querySelector('h1');
+
+    if (username) {
+        welcomeHeader.textContent = `Welcome back, ${username}!`;
+    } else {
+       // back to login page if they aren't properly logged in
+        window.location.href = 'login.html';
+    }
+});
--- src/main/java/com/author/book_finder/dto/LoginRequest.java
+++ src/main/java/com/author/book_finder/dto/LoginRequest.java
@@ -4,6 +4,13 @@ public class LoginRequest {
     private String username;
     private String password;
 
+    public LoginRequest() {}
+
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     // Getters and Setters
     public String getUsername() {
         return username;
